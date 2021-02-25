06648072b02930e1779029c5034146999e7682d7
Risa Takata <beat1422@icloud.com>
2021-02-25 12:49:32 -0800

    DELETE: ハンバーガーメニューに関する不要コードの削除

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,3 @@
-// $(document).ready(function () {
-//   $(window).scroll(function () {
-//     // sticky navbar on scroll script
-//     if (this.scrollY > 20) {
-//       $(".header").addClass("sticky");
-//     } else {
-//       $(".header").removeClass("sticky");
-//     }
-//   });
-
-//   // toggle menu/navbar script
-//   $(".menu-btn").click(function () {
-//     $(".gnav").toggleClass("active");
-//     $(".menu-btn").toggleClass("active");
-//   });
-// });
 $(document).ready(function () {
   $(window).scroll(function () {
     // sticky navbar on scroll script
@@ -47,42 +31,5 @@ $(document).ready(function () {
   $(".menu-btn").click(function () {
     $(".gnav__list").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
-  });
-
-  // typing text animation script
-  var typed = new Typed(".typing", {
-    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-
-  var typed = new Typed(".typing-2", {
-    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-
-  // owl carousel script
-  $(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      600: {
-        items: 2,
-        nav: false,
-      },
-      1000: {
-        items: 3,
-        nav: false,
-      },
-    },
   });
 });
