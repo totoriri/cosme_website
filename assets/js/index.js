6b29b27f89e19f4ba2339ce2ca8aad7075ebef32
Risa Takata <beat1422@icloud.com>
2021-03-06 13:18:13 -0800

    DELETE: JQuery不要コードの削除

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,28 +10,6 @@ $(document).ready(function () {
       $("#price-link").attr("href", "#price--pc");
     }
   });
-  $(window).scroll(function () {
-    // sticky navbar on scroll script
-    if (this.scrollY > 20) {
-      $(".header").addClass("sticky");
-    } else {
-      $(".header").removeClass("sticky");
-    }
-
-    // scroll-up button show/hide script
-    if (this.scrollY > 500) {
-      $(".scroll-up-btn").addClass("show");
-    } else {
-      $(".scroll-up-btn").removeClass("show");
-    }
-  });
-
-  // slide-up script
-  $(".scroll-up-btn").click(function () {
-    $("html").animate({ scrollTop: 0 });
-    // removing smooth scroll on slide-up button click
-    $("html").css("scrollBehavior", "auto");
-  });
 
   $(".gnav__item a").click(function () {
     // applying again smooth scroll on menu items click
